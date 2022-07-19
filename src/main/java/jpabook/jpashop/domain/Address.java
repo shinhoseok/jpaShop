88e960b745bfa649c1341febbb6d8255b585b10a
shinhoseok <masin84@naver.com>
2022-07-19 10:25:21 +0900

    설명주석

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -3,7 +3,9 @@ package jpabook.jpashop.domain;
 import lombok.Getter;
 
 import javax.persistence.Embeddable;
-
+//엔티티나 임베디드 타입은 기본생성자를 public 또는
+//protected로 설정해야한다. 무분별한 new를 막기위해 protectd로 설정하는것이
+//그나마 더 안전하다. JPA의 리플렉션을 위함
 @Embeddable
 @Getter
 public class Address {
